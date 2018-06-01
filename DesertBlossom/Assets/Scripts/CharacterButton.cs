302a2f14d1fa0fa534fd3f87d0128d4d6540a301
Ryota Ando <andoooo9.y.1127@gmail.com>
2018-06-01 17:03:36 +0900

    1stコミット。

---
+++ DesertBlossom/Assets/Scripts/CharacterButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterButton : MonoBehaviour
+{
+    private Button topBtn; // 矢印キー"↑"に対応するボタン
+    private Button rightBtn; // 矢印キー"→"に対応するボタン
+    private Button bottomBtn; // 矢印キー"↓"に対応するボタン
+    private Button leftBtn; // 矢印キー"←"に対応するボタン
+
+    // 各ボタンのプロパティを作成
+    public Button TopBtn { get { return topBtn; } }
+    public Button RightBtn { get { return topBtn; } }
+    public Button BottomBtn { get { return topBtn; } }
+    public Button LeftBtn { get { return topBtn; } }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ DesertBlossom/Assets/Scripts/ModeSelect.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+// モード選択を行うクラス
+public class ModeSelect : MonoBehaviour
+{
+    // モードの番号(1:一人プレイモード, 2:二人プレイモード)
+    public static int mode { get; set; }
+
+    private Button selectBtn; // 選択中のボタン
+
+    // Use this for initialization
+    void Start()
+    {
+        selectBtn = GameObject.Find("ModeSelect/Panel/Play_1").GetComponent<Button>();
+        selectBtn.Select();
+    }
+
+    /// <summary>
+    /// クリックしたボタンによってmodeの値を変更
+    /// </summary>
+    /// 
+    /// <param name="number">ボタンが持つパラメータ</param>
+    public void OnClick(int number)
+    {
+        switch (number)
+        {
+            case 1:
+            case 2:
+                mode = number;
+                SceneManager.LoadScene("CharacterSelect");
+                break;
+            default:
+                break;
+        }
+    }
+}
