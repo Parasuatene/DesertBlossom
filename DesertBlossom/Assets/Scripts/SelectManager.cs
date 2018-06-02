24f7975ff20d74aac198685a148bb6caeb86e855
Ryota Ando <andoooo9.y.1127@gmail.com>
2018-06-03 01:55:14 +0900

    入力キーによってアイコンの画像が変更する処理を実装。

--- DesertBlossom/Assets/Scripts/SelectManager.cs
+++ DesertBlossom/Assets/Scripts/SelectManager.cs
@@ -5,17 +5,50 @@ using UnityEngine.UI;
 
 public class SelectManager : MonoBehaviour
 {
-    [SerializeField]Image firstIcon; // 初めに選択状態にするアイコン
-    
+    [SerializeField] GameObject curIcon; // 選択状態のアイコン（初期アイコンはインスペクター上から設定）
+    private GameObject prevIcon;
+    private CharacterIcon cIcon;
+
     // Use this for initialization
     void Start()
     {
-        firstIcon.GetComponent<CharacterIcon>().setImage();
+        cIcon = curIcon.GetComponent<CharacterIcon>();
+        curIcon.GetComponent<Image>().sprite = cIcon.select_1; // Playerの選択状態の画像にする
     }
 
     // Update is called once per frame
     void Update()
     {
+        // 入力キーに従って、対応するアイコンをセット
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            GameObject topIcon = curIcon.GetComponent<CharacterIcon>().TopIcon;
+            changeIcon(topIcon);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GameObject rightIcon = curIcon.GetComponent<CharacterIcon>().RightIcon;
+            changeIcon(rightIcon);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            GameObject bottomIcon = curIcon.GetComponent<CharacterIcon>().BottomIcon;
+            changeIcon(bottomIcon);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GameObject leftIcon = curIcon.GetComponent<CharacterIcon>().LeftIcon;
+            changeIcon(leftIcon);
+        }
+    }
 
+    private void changeIcon(GameObject typeIcon)
+    {
+        if (typeIcon != null)
+        {
+            prevIcon = curIcon;
+            curIcon = typeIcon;
+            curIcon.GetComponent<CharacterIcon>().setIcon(1, prevIcon);
+        }
     }
 }
