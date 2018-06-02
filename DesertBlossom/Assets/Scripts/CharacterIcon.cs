accf5fe9bf249636b383c6befc6592739e331b57
Ryota Ando <andoooo9.y.1127@gmail.com>
2018-06-03 01:54:28 +0900

    各アイコンの型をImageからGameObjectに変更。

--- DesertBlossom/Assets/Scripts/CharacterIcon.cs
+++ DesertBlossom/Assets/Scripts/CharacterIcon.cs
@@ -5,24 +5,24 @@ using UnityEngine.UI;
 
 public class CharacterIcon : MonoBehaviour
 {
-    private Image topIcon; // 矢印キー"↑"に対応するアイコン
-    private Image rightIcon; // 矢印キー"→"に対応するアイコン
-    private Image bottomIcon; // 矢印キー"↓"に対応するアイコン
-    private Image leftIcon; // 矢印キー"←"に対応するアイコン
+    private GameObject topIcon; // 矢印キー"↑"に対応するアイコン
+    private GameObject rightIcon; // 矢印キー"→"に対応するアイコン
+    private GameObject bottomIcon; // 矢印キー"↓"に対応するアイコン
+    private GameObject leftIcon; // 矢印キー"←"に対応するアイコン
 
     // 各アイコンのプロパティを作成
-    public Image TopIcon { get { return topIcon; } }
-    public Image RightIcon { get { return rightIcon; } }
-    public Image BottomIcon { get { return bottomIcon; } }
-    public Image LeftIcon { get { return leftIcon; } }
+    public GameObject TopIcon { get { return topIcon; } }
+    public GameObject RightIcon { get { return rightIcon; } }
+    public GameObject BottomIcon { get { return bottomIcon; } }
+    public GameObject LeftIcon { get { return leftIcon; } }
 
     private bool onPlayer1; // プレイヤー1にセットされているかどうか
     private bool onPlayer2; // プレイヤー2にセットされているかどうか
 
     // セットするイメージ画像
     [SerializeField] Sprite select_0; // デフォルトのとき
-    [SerializeField] Sprite select_1; // プレイヤー1が選択しているとき
-    [SerializeField] Sprite select_2; // プレイヤー2が選択しているとき
+    public Sprite select_1; // プレイヤー1が選択しているとき
+    public Sprite select_2; // プレイヤー2が選択しているとき
     [SerializeField] Sprite select_3; // どちらも選択しているとき
 
     // Use this for initialization
