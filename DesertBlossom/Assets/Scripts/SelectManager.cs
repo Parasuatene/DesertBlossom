24fe08041eecd98ad1f15c2d422dae873c151d48
Ryota Ando <andoooo9.y.1127@gmail.com>
2018-06-03 02:10:18 +0900

    GetKeyからGetKeyDownに変更。

--- DesertBlossom/Assets/Scripts/SelectManager.cs
+++ DesertBlossom/Assets/Scripts/SelectManager.cs
@@ -20,22 +20,22 @@ public class SelectManager : MonoBehaviour
     void Update()
     {
         // 入力キーに従って、対応するアイコンをセット
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             GameObject topIcon = currIcon.GetComponent<CharacterIcon>().TopIcon;
             changeIcon(topIcon);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             GameObject rightIcon = currIcon.GetComponent<CharacterIcon>().RightIcon;
             changeIcon(rightIcon);
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             GameObject bottomIcon = currIcon.GetComponent<CharacterIcon>().BottomIcon;
             changeIcon(bottomIcon);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             GameObject leftIcon = currIcon.GetComponent<CharacterIcon>().LeftIcon;
             changeIcon(leftIcon);
