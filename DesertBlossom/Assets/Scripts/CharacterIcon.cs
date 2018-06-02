3710ecc88886409f988cde84a1f121864e4fbfa6
Ryota Ando <andoooo9.y.1127@gmail.com>
2018-06-03 01:23:43 +0900

    アイコンを管理する関数を追加。

--- DesertBlossom/Assets/Scripts/CharacterIcon.cs
+++ DesertBlossom/Assets/Scripts/CharacterIcon.cs
@@ -16,6 +16,9 @@ public class CharacterIcon : MonoBehaviour
     public Image BottomIcon { get { return bottomIcon; } }
     public Image LeftIcon { get { return leftIcon; } }
 
+    private bool onPlayer1; // プレイヤー1にセットされているかどうか
+    private bool onPlayer2; // プレイヤー2にセットされているかどうか
+
     // セットするイメージ画像
     [SerializeField] Sprite select_0; // デフォルトのとき
     [SerializeField] Sprite select_1; // プレイヤー1が選択しているとき
@@ -34,8 +37,49 @@ public class CharacterIcon : MonoBehaviour
 
     }
 
-    public void setImage()
+    // 選択アイコンが切り替わる度にonPlayerを変更。
+    public void setIcon(int playerType, GameObject prevIcon)
     {
-        
+        CharacterIcon cIcon = prevIcon.GetComponent<CharacterIcon>();
+
+        switch (playerType)
+        {
+            case 1:
+                onPlayer1 = true;
+                cIcon.onPlayer1 = false;
+                break;
+            case 2:
+                onPlayer2 = true;
+                cIcon.onPlayer2 = false;
+                break;
+            default:
+                break;
+        }
+
+        setSprite(gameObject);
+        setSprite(prevIcon);
+    }
+
+    // アイコンの画像を適切なものに変更する
+    private void setSprite(GameObject changeIcon)
+    {
+        Image iconImg = changeIcon.GetComponent<Image>();
+
+        if (onPlayer1 == true && onPlayer2 == true) // どちらもが選択状態の場合
+        {
+            iconImg.sprite = select_3;
+        }
+        else if (onPlayer1 == true) // Player1が選択状態の場合
+        {
+            iconImg.sprite = select_1;
+        }
+        else if (onPlayer2 == true) // Player2が選択状態の場合
+        {
+            iconImg.sprite = select_2;
+        }
+        else
+        {
+            iconImg.sprite = select_0;
+        }
     }
 }
