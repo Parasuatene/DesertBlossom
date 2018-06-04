bb604ea78d0316f427f1b0ddaa55a7a40733727e
Ryota Ando <andoooo9.y.1127@gmail.com>
2018-06-05 03:32:13 +0900

    キャラクターをテスト用に追加。

---
+++ DesertBlossom/Assets/Scripts/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    [SerializeField] string name; // キャラクターの名前
+
+    public string Name { get { return name; } }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
